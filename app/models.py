a2a28e403501c466f86d6d3184bdbd0af8c4a7e6
Quentin Canaguier <quentin.canaguier@gmail.com>
2026-09-09 18:32:59 -0400

    database models added and alembic migrations done

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(String(50))
+    email: Mapped[str] = mapped_column(String(250), unique=True)
+
+
